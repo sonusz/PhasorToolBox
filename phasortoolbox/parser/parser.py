602da02eb638961519a156d55f9279672c18cbb2
s <waved1980@gmail.com>
2017-09-27 17:12:17 -0400

    Simplfy parser class, and improves doc string

--- phasortoolbox/parser/parser.py
+++ phasortoolbox/parser/parser.py
@@ -6,23 +6,42 @@ from .minicfg import MiniCfgs
 
 
 class parser(object):
-    def __init__(self, _bytes: bytes = None, raw_cfg_pkt: bytes = None):
-        """
-        This is used to parse bytes packet or stream.
-        :type cfg_pkt: configuration packet raw data
-        """
+    """ A Parser that parses synchrphasor messages defined by IEEE Std
+    C37.118.2-2011.
 
-        self._raw_data = _bytes
+    Note:
+        When parsing a stream, the parser automatically detects and stores
+        configuration messages and use the most recent received configuration
+        message to parse the following data messages. Multiple synchrophasor
+        message streams can be parsed using one parser instance. The parser
+        identifies each synchrophasor message stream by using its IDCODE and
+        then apply the corresponded configuration message to parse the data
+        message.
+
+    Example:
+    # The first example creates a command message and then creates a parser to
+    parse the message.
+        my_msg = command(CMD='off') # Creates a command message.
+        print(my_msg)               # Just to show the content.
+        my_parser = parser()        # Creates a parser
+        my_msgs = my_parser.parse(msg)  # Parse the previously created mseeage\
+                                        and returns a list of parsed messages
+        print(my_msgs[0].data.cmd.name) # Print the contant of the command
+    """
+
+    def __init__(self, raw_cfg_pkt: bytes = None):
         self._mini_cfgs = MiniCfgs()
         if raw_cfg_pkt:
             _io = KaitaiStream(BytesIO(raw_cfg_pkt))
             while not _io.is_eof():
                 pkt = Common(_io)
                 self._mini_cfgs.add_cfg(pkt.raw_pkt)
-        if self._raw_data:
-            self.parse(self._raw_data)
 
     def parse(self, raw_byte: bytes):
+        """Parse synchrphasor messages
+
+
+        """
         message = []
         self._raw_data = raw_byte
         _io = KaitaiStream(BytesIO(self._raw_data))
