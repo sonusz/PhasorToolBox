34d6757e9ae71db7bbaffc9366ac7eaaee66c297
s <waved1980@gmail.com>
2017-09-27 17:11:26 -0400

    Improves doc string

--- phasortoolbox/message.py
+++ phasortoolbox/message.py
@@ -7,19 +7,22 @@ class message(bytes):
     """This object returns a synchrphasor message in bytes.
     It would be easyer to use classes that inheritaged from this class,
     such as command(), data(), cfg2()
+
     Note:
         This creates a Version 2 (0010) message defined by IEEE Std
         C37.118.2-2011.
         TIME, TQ_FLAGS and MSG_TQ are used to generate SOC and FRACSEC of the
         synchrophasor message.
+
     Example:
         my_msg = message()
         my_msg = message(DATA=b'\x00\x00')
+
     Args:
         SYNC (bytes): Frame synchronization word. Defaults to b'\xaa\x00'.
         IDCODE (int): Data stream ID number. Defaults to 0.
         TIME (float): Epoch time. Defaults to current time.
-        TQ_FLAGS (str): Four bytes string indicates time quatity flags. 
+        TQ_FLAGS (str): Four bytes string indicates time quatity flags.
                         Defaults to '0000'
         MSG_TQ (str): Four bytes string indicates message time quatity (
                       MSG_TQ). Defaults to '1111'
@@ -61,21 +64,23 @@ class command(message):
         synchrophasor message.
 
     Example:
-        my_msg = command(IDCODE=0, CMD='on') # Turn on transmission of data 
+        my_msg = command(IDCODE=0, CMD='on') # Turn on transmission of data
         frames.
-        my_msg = command(IDCODE=0, CMD='ext', EXT = b'User defined message') 
+        my_msg = command(IDCODE=0, CMD='ext', EXT = b'User defined message')
         # An extended command frame with user defined message.
+
     Args:
-        IDCODE (int)    :Data stream ID number. Defaults to 1. 1–65534 (0 and 65535 are reserved).
+        IDCODE (int)    :Data stream ID number. Defaults to 1. 1–65534 (0 and
+        65535 are reserved).
         TIME (float)    :Epoch time. Defaults to current time.
-        TQ_FLAGS (str)  :Four bytes string indicates time quatity flags. 
+        TQ_FLAGS (str)  :Four bytes string indicates time quatity flags.
                         Defaults to '0000'
         MSG_TQ (str)    :Four bytes string indicates message time quatity (
                         MSG_TQ). Defaults to '1111'
         TIME_BASE (int) :TIME_BASE. Defaults to 16777215.
-        USER_DEF (str)  :Four bytes string codes designated by user. Defaults 
+        USER_DEF (str)  :Four bytes string codes designated by user. Defaults
                         to '0000'
-        CMD (str)       :Commands designated by user. 
+        CMD (str)       :Commands designated by user.
                         Options are:
                             'off' :Turn off transmission of data frames.
                             'on'  :Turn on transmission of data frames.
@@ -84,7 +89,7 @@ class command(message):
                             'cfg2':Send CFG-2 frame.
                             'cfg3':Send CFG-3 frame (optional command).
                             'ext':Extended frame.
-        EXT (bytes)     :Extended frame data, 16-bit words, 0 to 65518 
+        EXT (bytes)     :Extended frame data, 16-bit words, 0 to 65518
                         bytes as indicated by frame size, data user defined.
     """
 
@@ -115,5 +120,5 @@ class command(message):
             CommandCode[CMD].to_bytes(1, 'big')
 
 
-#my_msg = command(IDCODE=0, CMD='ext', EXT=b'User defined message')
-#print(P.parse(my_msg)[0].data.ext)
+# my_msg = command(IDCODE=0, CMD='ext', EXT=b'User defined message')
+# print(P.parse(my_msg)[0].data.ext)
