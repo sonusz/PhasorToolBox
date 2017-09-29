fe46020a5b627dacda3e48e751e762a9491f455b
s <waved1980@gmail.com>
2017-09-29 00:47:21 -0400

    Improves message module

--- phasortoolbox/message.py
+++ phasortoolbox/message.py
@@ -5,63 +5,51 @@ import binascii
 
 
 class message(bytes):
-    """This object returns a synchrphasor message in bytes.
-    It would be easyer to use classes that inheritaged from this class,
+    """This object returns a synchrophasor message in bytes.
+    It would be easier to use classes that inherited from this class,
     such as command(), data(), cfg2()
 
     Note:
-        This creates a Version 2 (0010) message defined by IEEE Std
-        C37.118.2-2011.
+        This creates an empty Version 1 (0001) DATA frame defined by IEEE Std
+        C37.118.2-2011 by default. Only the CFG-3 frame is Version 2 (0010)
         TIME, TQ_FLAGS and MSG_TQ are used to generate SOC and FRACSEC of the
         synchrophasor message.
 
     Example:
         my_msg = message()
-        my_msg = message(DATA=b'\x00\x00')
+        my_msg = message(SYNC=b'\xaa\x01', IDCODE=1, DATA=b'\x00\x00')
 
     Args:
-        SYNC (bytes): Frame synchronization word. Defaults to b'\xaa\x00'.
+        SYNC (bytes): Frame synchronization word. Defaults to b'\xaa\x01'.
         IDCODE (int): Data stream ID number. Defaults to 0.
         TIME (float): Epoch time. Defaults to current time.
-        TQ_FLAGS (str): Four bytes string indicates time quatity flags.
+        TQ_FLAGS (str): Four bytes string indicates time quality flags.
                         Defaults to '0000'
-        MSG_TQ (str): Four bytes string indicates message time quatity (
+        MSG_TQ (str): Four bytes string indicates message time quality (
                       MSG_TQ). Defaults to '1111'
         TIME_BASE (int): TIME_BASE. Defaults to 16777215.
     """
 
     def __new__(
-            self, SYNC=b'\xaa\x00', IDCODE=1,
+            self, SYNC=b'\xaa\x01', IDCODE=1,
             TIME='NOW', TQ_FLAGS='0000', MSG_TQ='1111',
             TIME_BASE=16777215, DATA=b''):
         FRAMESIZE = (len(DATA) + 16).to_bytes(2, 'big')
-        SOC_FRACSEC = self.soc_fracsec(
-            self, TIME=TIME, TIME_QUALITY_FLAGS=TQ_FLAGS + MSG_TQ,
-            TIME_BASE=TIME_BASE)
-        raw_data = SYNC + FRAMESIZE + \
-            IDCODE.to_bytes(2, 'big') + SOC_FRACSEC + DATA
-        raw_data += self.checksum(self, raw_data)
-        return super(message, self).__new__(self, raw_data)
-
-    def soc_fracsec(
-            self, TIME, TIME_QUALITY_FLAGS, TIME_BASE):
-        """
-        Check IEEE Std C37.118.2-2011 Table 3 Table 4 for Time quality flags
-        """
         if TIME == 'NOW':
             TIME = time.time()
-        return int(TIME).to_bytes(4, 'big') \
-            + int(TIME_QUALITY_FLAGS, 2).to_bytes(1, 'big') \
-            + int(TIME % 1 * TIME_BASE).to_bytes(3, 'big')
-
-    def checksum(self, raw_data):
-        return binascii.crc_hqx(raw_data, -1).to_bytes(2, 'big')
+        SOC_FRACSEC = b''.join((
+            int(TIME).to_bytes(4, 'big'), int(TQ_FLAGS + MSG_TQ, 2).to_bytes(
+                1, 'big'), int(TIME % 1 * TIME_BASE).to_bytes(3, 'big')))
+        raw_data = b''.join((SYNC, FRAMESIZE,
+                             IDCODE.to_bytes(2, 'big'), SOC_FRACSEC, DATA))
+        raw_data += binascii.crc_hqx(raw_data, -1).to_bytes(2, 'big')
+        return super(message, self).__new__(self, raw_data)
 
 
 class command(message):
     """This object returns a command message in bytes.
     Note:
-        This creates a Version 2 (0010) command message defined by IEEE Std
+        This creates a Version 1 (0001) command message defined by IEEE Std
         C37.118.2-2011.
         TIME, TQ_FLAGS and MSG_TQ are used to generate SOC and FRACSEC of the
         synchrophasor message.
@@ -76,9 +64,9 @@ class command(message):
         IDCODE (int)    :Data stream ID number. Defaults to 1. 1–65534 (0 and
         65535 are reserved).
         TIME (float)    :Epoch time. Defaults to current time.
-        TQ_FLAGS (str)  :Four bytes string indicates time quatity flags.
+        TQ_FLAGS (str)  :Four bytes string indicates time quality flags.
                         Defaults to '0000'
-        MSG_TQ (str)    :Four bytes string indicates message time quatity (
+        MSG_TQ (str)    :Four bytes string indicates message time quality (
                         MSG_TQ). Defaults to '1111'
         TIME_BASE (int) :TIME_BASE. Defaults to 16777215.
         USER_DEF (str)  :Four bytes string codes designated by user. Defaults
@@ -96,23 +84,11 @@ class command(message):
                         bytes as indicated by frame size, data user defined.
     """
 
-    def __new__(self, IDCODE=1, CMD='off', 
+    def __new__(self, IDCODE=1, CMD='off',
                 TIME='NOW', TQ_FLAGS='0000', MSG_TQ='1111',
                 TIME_BASE=16777215,
                 USER_DEF='0000', EXT=b''
                 ):
-        SYNC = b'\xaaA'
-        if CMD == 'cfg3':
-            SYNC = b'\xaaB'
-        return super().__new__(
-            self, SYNC=SYNC, IDCODE=IDCODE,
-            TIME=TIME, TQ_FLAGS=TQ_FLAGS, MSG_TQ=MSG_TQ,
-            TIME_BASE=16777215,
-            DATA=self._command_code(
-                self, USER_DEF, CMD) + EXT
-        )
-
-    def _command_code(self, USER_DEF, CMD):
         CommandCode = {
             'off': 1,
             'on': 2,
@@ -122,5 +98,10 @@ class command(message):
             'cfg3': 6,
             'ext': 8
         }
-        return int('0000' + USER_DEF, 2).to_bytes(1, 'big') + \
-            CommandCode[CMD].to_bytes(1, 'big')
+        return super().__new__(
+            self, SYNC=b'\xaaA', IDCODE=IDCODE,
+            TIME=TIME, TQ_FLAGS=TQ_FLAGS, MSG_TQ=MSG_TQ,
+            TIME_BASE=16777215,
+            DATA=b''.join((int('0000' + USER_DEF, 2).to_bytes(1, 'big'),
+                           CommandCode[CMD].to_bytes(1, 'big'), EXT))
+        )
