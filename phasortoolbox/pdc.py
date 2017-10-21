5c7e23bcd4b79a8ff83a6d9d11f4852b9a6e82dd
s <waved1980@gmail.com>
2017-10-21 17:18:17 -0400

    Bug fix

--- phasortoolbox/pdc.py
+++ phasortoolbox/pdc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 import bisect
-from collections import deque defaultdict
+from collections import deque, defaultdict
 import asyncio
 from concurrent import futures
 from phasortoolbox import Parser
