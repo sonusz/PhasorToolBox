5f4b265ef2ed36a4dfe8b14f0b64adf29066d356
Xingsi <zhongxingsi@gmail.com>
2018-04-12 16:10:13 -0400

    Improved pdc.py

--- phasortoolbox/pdc.py
+++ phasortoolbox/pdc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 import bisect
-from collections import deque, defaultdict
+from collections import defaultdict
 import asyncio
 from concurrent import futures
 from phasortoolbox import Parser
@@ -16,28 +16,30 @@ class PDC(object):
     Example:
     my_pdc = PDC()
 
+    This class aligns in coming synchrophasor messages using time tags.
+
 
     """
 
     def __init__(
         self,
-        CALLBACK=None,
-        BUF_SIZE=1,
-        FILTER={'data'},
-        WAIT_TIME=0.1,
+        CALLBACK=None,  # Function which will be executed while data available, defined by user
+        BUF_SIZE=1,  # Number of historical data returned to CALLBACK
+        returnNone=False,
+        # returnNone determines if partially received record should be discarded when timeout. Record will be discarded on False; None will be used as place holder and return partially received record on True.
+        WAIT_TIME=0.1,  # The max time to wait for each time tag, will return None if returnNone is True
+        count=0,
+        FILTER={'data'},  # Type of PMU message will be processed
         loop: asyncio.AbstractEventLoop()=None,
         executor: futures.Executor()=None,
-        step_time=0.01,
-        returnNone=False,
-        count=0
-        # Partially Timeout time stamps will be discarded on False
-        # None will be returned for timeout data on True.
+        step_time=0.01  # The time to wait for asyncio when no data coming in
+        # Count of data send before stop. 0 for send forever.
     ):
         self.WAIT_TIME = WAIT_TIME
         self.FILTER = FILTER
         self.BUF_SIZE = BUF_SIZE
         self.step_time = step_time
-        self.buf_time_out = self.BUF_SIZE * self.WAIT_TIME * 2
+        self.buf_time_out = self.BUF_SIZE * self.WAIT_TIME * 10
         self._input_list = []
         self._output_list = []
         self._input_queue = asyncio.Queue()
@@ -50,97 +52,79 @@ class PDC(object):
             self.CALLBACK = CALLBACK
         self.returnNone = returnNone
         self.count = count
-
+        
     async def run(self, count=None):
         if not self._input_list:
             print('No input defined.')
             return
-        self._ordered_idcode_list = []
+        self._idcode_list = []
         for _input in self._input_list:
-            self._ordered_idcode_list.append(_input.IDCODE)
-        self._ordered_idcode_list.sort()
+            self._idcode_list.append(_input.IDCODE)
         if not callable(self.CALLBACK):
             raise TypeError("Input must be a function, "
                             "not {!r}".format(type(self.CALLBACK)))
         self._buf = {}
-        self._buf_index = deque()
+        self._buf_index = []
         if not count:
             count = self.count
         while True:
             try:
                 ###############################################################
-                """Check which data can be send 
-                All data are kept in an dictionary. A valid record also
-                contains the earlest arrive time and a flag indicated if the
-                record has been sent previously. The newest record returned to
-                the user CALLBACK must be a new record that has never been
-                sent before. Thus, first check if the newest data has beend
-                sent beforefrom the newest arrived data.
+                """The following chunk of code checks which data can be send
+                and generate a list of ordered time tags.
+                The returned data should be a list of ordered and aligned
+                record. The first record in the returned list should never
+                been sent before. The following record needs to be valid or
+                has been send before.
+                A flag (_buf[time_tag]['sent']) is used to indicate if the
+                record has been sent before. True will be assigned if the
+                record has been sent before (_buf[time_tag]['sent']==True).
                  """
                 _temp_send_list = []
                 _time_out_by = time.time() - self.WAIT_TIME
-                for time_tag in reversed(self._buf_index):
-                    if len(_temp_send_list) == self.BUF_SIZE:
-                        break
-                    if (
-                        (len(_temp_send_list) > 0) and
-                        (
-                            self._buf[time_tag]['sent'] or
-                            (
-                                len(self._buf[time_tag]) - 2 ==
-                                len(self._input_list)
-                            ) or
-                            (
-                                self.returnNone and
-                                (
-                                    self._buf[time_tag]['_arrtime'] <
-                                    _time_out_by
-                                )
+                # This is a time point in the past, time tags earlier/smaller
+                # than this point will be sent or discarded.
+                if len(self._buf_index) > 0:
+                    _newest_tag = self._buf_index[-1]
+                    if (self._buf[_newest_tag]['sent'] is not True
+                        # The record with newest time tag haven't been sent
+                        and (len(self._buf[_newest_tag]) - 2 ==
+                            len(self._input_list)
+                            # Data ready to send
+                            or (self.returnNone
+                                and self._buf[_newest_tag]['_arrtime'] <
+                                _time_out_by
+                                )  # Data time out
                             )
-                        )
-                    ):
-                        _temp_send_list.append(time_tag)
-                        # Valid to send, also the first one already found.
-                        continue
-                    elif (
-                        (
-                            len(_temp_send_list) == 0
-                        ) and
-                        (
-                            not self._buf[time_tag]['sent']
-                        ) and
-                        (
-                            (
-                                (len(self._buf[time_tag]) - 2) ==
-                                len(self._input_list)
-                            ) or
-                            (
-                                self.returnNone and
-                                (
-                                    self._buf[time_tag]['_arrtime'] <
-                                    _time_out_by
-                                )
-                            )
-                        )
-                    ):
-                        # The fist item in the list must be the newest recored
-                        # valid to send and has never been sent before.
-                        _temp_send_list.append(time_tag)
-                        continue
-                    elif (
-                        (len(_temp_send_list) == 0) and
-                        (self._buf[time_tag]['sent'])
-                    ):
-                        # The newest recored valid to send has been sent
-                        # before, no need to do anything.
-                        break
+                        ):
+                        _temp_send_list.append(_newest_tag)
+                        for time_tag in reversed(self._buf_index[:-1]):
+                            if len(_temp_send_list) == self.BUF_SIZE:
+                                # Exit this loop when get enough data
+                                break
+                            if (self._buf[time_tag]['sent'] is True
+                                # The record have been sent before
+                                or len(self._buf[time_tag]) - 2 ==
+                                            len(self._input_list)
+                                # Data ready to send
+                                or self.returnNone
+                                # Data time out allow to send
+                                ):
+                                _temp_send_list.append(time_tag)
+
+                ###############################################################
+                """ The following chunk of code prepare buffer_msgs for user's
+                function. If multiple time tags are returned, the time tags
+                are ordered in the receiving order (The newest received will
+                be the last one in the returned list).
+                """
                 if len(_temp_send_list) == self.BUF_SIZE:
                     # Will not do anything if not enough data to send
-                    # Prepare send msgs
+                    # Prepare send msgs and call user defined function
                     buffer_msgs = [
                         [
                             self._buf[time_tag][idcode] for idcode in
-                            self._ordered_idcode_list
+                            self._idcode_list
                         ]
                         for time_tag in reversed(_temp_send_list)
                     ]
@@ -165,10 +149,9 @@ class PDC(object):
                 if len(_temp_send_list) == self.BUF_SIZE:
                     # Remove all data until the last one sent
                     for time_tag in self._buf_index:
-                        _del_list.append(time_tag)
                         if time_tag < _temp_send_list[-1]:
-                            continue
-                        elif time_tag == _temp_send_list[-1]:
+                            _del_list.append(time_tag)
+                        else:
                             break
                 else:
                     # Remove all data until buffer time out
@@ -185,7 +168,7 @@ class PDC(object):
                 ###############################################################
                 """Get all data from queue
                 If user's CALLBACK function is too slow, queue size will keep
-                increase. Get all data from queue if queue have pendding
+                increase. Get all data from queue if queue have pending
                 data. If user's CALLBACK function is fast enough, then wait
                 until item available in queue.
                 """
@@ -216,3 +199,4 @@ class PDC(object):
 
     async def clean_up(self):
         pass
+
