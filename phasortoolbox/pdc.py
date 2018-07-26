125cb83f11ae63b11ed340243d81bf2b3b6218df
s <waved1980@gmail.com>
2018-07-26 10:19:13 -0400

    Put Synchrophasor in a seperate file

--- phasortoolbox/pdc.py
+++ phasortoolbox/pdc.py
@@ -2,26 +2,12 @@
 import time
 import bisect
 import asyncio
-from collections import UserList, defaultdict
+from collections import defaultdict
 from concurrent.futures import Executor, ThreadPoolExecutor
-from phasortoolbox import Client
+from phasortoolbox import Client, Synchrophasor
 import logging
 
 LOG=logging.getLogger('phasortoolbox.pdc')
-
-
-class Synchrophasor(UserList):
-    """
-    time_tag is the time tag of the synchrophasor message
-    arr_time is the unix time that the last data frame for the synchrophasor received 
-    perf_counter is used the check the performace
-    """
-    def __init__(self, list_, time_tag, arr_time, perf_counter):
-        super(Synchrophasor, self).__init__(list_)
-        self.time = time_tag
-        self.arr_time = arr_time
-        self.perf_counter = perf_counter 
-
 
 class PDC(object):
     """docstring for PDC
